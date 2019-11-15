1c8d19b378bb1c0f9c2fd9c02ef1301e9ab0d02d
darcy.diaz <darcy.diaz@CLOUDDEV-E12.CLOUDDEVELOPMENT.PH>
2019-11-15 16:30:07 +0800

    Runner Test

--- src/main/java/runners/Runner.java
+++ src/main/java/runners/Runner.java
@@ -10,7 +10,7 @@ import cucumber.api.junit.Cucumber;
 	monochrome = true, // For code readability.
 	strict = true, // Always set to true to check every undefined/pending steps.
 	dryRun = false, // Checking of steps if implemented or not.
-	tags = { "@Sprint1" }, // @WIP = Work in Progress. @Regression = For Repeating Testing. @RegressionFail = Comment this line for testing multiple feature files.
+	tags = { "@Sprint" }, // @WIP = Work in Progress. @Regression = For Repeating Testing. @RegressionFail = Comment this line for testing multiple feature files.
 	// features = "src/test/resources/features/" // Features used for Regression.
 	// features = "src/test/resources/features/Sprint 1/MW - 25 - Single.feature" // Features used for Regression.
 	features = "src/test/resources/features/Sprint 1/MW - 42.feature" // Feature file to use for WIP.
